4a1da30d8ba70f5bc1d66180e07f4baa976f8299
Joao Francisco <jfsousacarvalho@gmail.com>
2023-09-11 22:52:26 -0300

    firts commit - everthing ok

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,113 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly ITagRepository tagRepository;
+
+        public AdminTagsController(ITagRepository tagRepository)
+        {
+            this.tagRepository = tagRepository;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ActionName("Add")]
+        public async Task<IActionResult> Add(AddTagRequest addTagRequest)
+        {
+            //Maping AddTagRequest to the Tag Domain Model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            await tagRepository.AddAsync(tag);
+
+            return RedirectToAction("List");
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [ActionName("List")]
+        public async Task<IActionResult> List()
+        {
+            // use DbContext to read the tags
+            var tags = await tagRepository.GetAllAsync();
+
+            return View(tags);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var tag = await tagRepository.GetAsync(id);
+
+            if (tag != null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName,
+                };
+
+                return View(editTagRequest);
+            }
+            return View(null);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName,
+            };
+
+            var updatedTag = await tagRepository.UpdateAsync(tag);
+
+            if (updatedTag != null)
+            {
+                // show success notification
+                return RedirectToAction("List");
+            }
+
+            // show error notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<ActionResult> Delete(EditTagRequest editTagRequest)
+        {
+            var deletedTag = await tagRepository.DeleteAsync(editTagRequest.Id);
+
+            if (deletedTag != null)
+            {
+                // show success notification
+                return RedirectToAction("List");
+            }
+
+            // show an error notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+    }
+}
---
+++ Bloggie.Web/Data/AuthDbContext.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // seed roles (user, admin, superAdmin
+
+            var adminRoleId = "006b7cb5-a512-4d8f-9428-38aef588da84";
+            var superAdminRoleId = "7c421cd6-0c73-4677-992e-7bebb947c03b";
+            var userRoleId = "743dd482-9203-4660-9d0d-ad49971c863f";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "Admin",
+                    Id = adminRoleId,
+                    ConcurrencyStamp = adminRoleId
+                },
+                new IdentityRole
+                {
+                    Name = "SuperAdmin",
+                    NormalizedName = "SuperAdmin",
+                    Id = superAdminRoleId,
+                    ConcurrencyStamp = superAdminRoleId
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "User",
+                    Id = userRoleId,
+                    ConcurrencyStamp = userRoleId
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            // seed superAdmin
+            var superAdminId = "bc1f6436-d161-439d-9062-fc8fa6c65972";
+            var superAdminUser = new IdentityUser
+            {
+                UserName = "superadmin@bloggie.com",
+                Email = "superadmin@bloggie.com",
+                NormalizedEmail = "superadmin@bloggie.com".ToUpper(),
+                NormalizedUserName = "superadmin@bloggie.com".ToUpper(),
+                Id = superAdminId,
+            };
+
+            superAdminUser.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(superAdminUser, "Password@123");
+
+            builder.Entity<IdentityUser>().HasData(superAdminUser);
+
+            // add all roles to superAdmin
+            var superAdminRoles = new List<IdentityUserRole<string>>
+            {
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRoleId,
+                    UserId = superAdminId,
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = superAdminRoleId,
+                    UserId = superAdminId,
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = userRoleId,
+                    UserId = superAdminId,
+                }
+            };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
+        }
+    }
+}
